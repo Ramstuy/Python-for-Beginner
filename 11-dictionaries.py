6e17740fb015940f14aeda247ab152fec33451ef
Ramstuy <ikrhidayat@gmail.com>
2023-09-23 23:51:00 +0800

    + Added file 11-,12-.

---
+++ 11-dictionaries.py
@@ -0,0 +1,10 @@
+dicti = {
+    'name': 'Dude',
+    'age': 30,
+    'nationality': 'american',
+    'is_old': True,
+    'listedpep': ['Parsley', 'Doomsley']
+}
+# Mirip seperti list, tetapi menggunakan kurung kerawal
+x = dicti['age']
+print(x)
---
+++ 12-while-loops.py
@@ -0,0 +1,8 @@
+i = 1
+
+while i < 5 or i == 5:
+# var i lebih kecil atau sama dengan 5
+# syntax 'or' jika memenuhi salah satu kondisi maka code akan dijalankan
+    print(i)
+    i += 1
+# artinya nilai dari i akan di increment sebanyak 1
