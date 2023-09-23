bb86509c7eb095fc63e0c2596f7daeb99357233f
Ramstuy <ikrhidayat@gmail.com>
2023-09-23 21:11:44 +0800

    added 8-functions.py

---
+++ 8-functions.py
@@ -0,0 +1,7 @@
+def greetings(name, numb):
+    print('Welcome to Mobillejeng',name+', Hari ini adalah hari ke-'+str(numb)+'mu Disini')
+    print('Welcome '+name[1])
+
+name = input('What is your name: ')
+numb = input('How long youve been here: ')
+greetings(name, numb)
